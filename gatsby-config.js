47d792383579000aea2a6ab085919f4530975100
urgosxd <60458146+urgosxd@users.noreply.github.com>
2020-11-22 18:51:18 -0500

    asd

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,6 @@
 const { createProxyMiddleware } = require("http-proxy-middleware")
 
-require("dotenv").config({
-  path: `.env.production`,
-})
+require("dotenv").config()
 
 const blogQuery = `{
   allStrapiPosts {
@@ -41,18 +39,6 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
-    {
-      resolve: `gatsby-plugin-algolia`,
-      options: {
-        appId: process.env.ALGOLIA_APP_ID,
-        // Use Admin API key without GATSBY_ prefix, so that the key isn't exposed in the application
-        // Tip: use Search API key with GATSBY_ prefix to access the service from within components
-        apiKey: process.env.ALGOLIA_API_KEY,
-        indexName: process.env.ALGOLIA_INDEX_NAME, // for all queries
-        queries,
-        chunkSize: 10000, // default: 1000
-      },
-    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -82,6 +68,18 @@ module.exports = {
         apiURL: "https://strapi-josue.herokuapp.com", //"https://strapi2-heroku.herokuapp.com",
         queryLimit: 1000,
         contentTypes: ["posts"],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-algolia`,
+      options: {
+        appId: process.env.ALGOLIA_APP_ID,
+        // Use Admin API key without GATSBY_ prefix, so that the key isn't exposed in the application
+        // Tip: use Search API key with GATSBY_ prefix to access the service from within components
+        apiKey: process.env.ALGOLIA_API_KEY,
+        indexName: process.env.ALGOLIA_INDEX_NAME, // for all queries
+        queries,
+        chunkSize: 10000, // default: 1000
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
